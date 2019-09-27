13f7f363793b4356cf93b5e189d278e6e687ec97
Sascha Picard <sascha.picard@fit.fichtner.de>
2019-09-27 16:49:48 +0200

    complete assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  buttonClicks: string[] = [];
 
+  logClick() {
+    this.buttonClicks.push(
+      new Date().toLocaleTimeString()
+    );
+  }
 }
