a684f014ddf3a808933e4aa9a827aaadcbade3a3
Sascha <sascha.picard@googlemail.com>
2019-10-08 23:16:44 +0200

    code along

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent implements OnInit {
+export class RecipeDetailComponent implements OnInit, OnChanges {
+
+  @Input() recipe: Recipe;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('onChanges called ', changes);
+  }
+
+
 
 }
