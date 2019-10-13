cc3da8b772ed6b3da9244323b99db706d0d7055e
Sascha <sascha.picard@googlemail.com>
2019-10-13 19:39:08 +0200

    bind to custom directive properties

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,16 +1,23 @@
-import {Directive, ElementRef, HostBinding, HostListener, Renderer2} from '@angular/core';
+import {Directive, HostBinding, HostListener, Input, OnInit} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective {
+export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor = 'transparent';
+  @Input() highlightColor = '#7CBEDD';
+
+  ngOnInit(): void {
+    this.backgroundColor = this.defaultColor;
+  }
+
   @HostBinding('style.backgroundColor') backgroundColor: string; // to which property of the hosting element we want to bind
 
   @HostListener('mouseenter') mouseover($event: Event) { // links dom event on our element to a custom callback
-    this.backgroundColor = '#7CBEDD';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave($event: Event) { // links dom event on our element to a custom callback
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 }
