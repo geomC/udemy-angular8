234af768fc28e43ad37d1ff24b6f61ef9aba5f4f
Sascha <sascha.picard@googlemail.com>
2019-10-15 21:00:39 +0200

    add way smarter solution of instructor

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,26 +1,13 @@
-import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appDropDown]'
 })
 export class DropdownDirective {
 
-  private CLASS_NAME = 'open'; // the bootstrap class to toggle
-  private toggled = false;
-
-  constructor(
-    private elementRef: ElementRef,
-    private renderer: Renderer2
-  ) {
-  }
+ @HostBinding('class.open') isOpen = false;
 
   @HostListener('click') toggleClass($event: Event) {
-    if (!this.toggled) {
-      this.renderer.addClass(this.elementRef.nativeElement, this.CLASS_NAME);
-    } else {
-      this.renderer.removeClass(this.elementRef.nativeElement, this.CLASS_NAME);
-    }
-
-    this.toggled = !this.toggled;
+    this.isOpen = !this.isOpen;
   }
 }
