d72364e5bd8158700fb5e4d9b0667ad307362dbf
Sascha <sascha.picard@googlemail.com>
2019-10-19 13:38:44 +0200

    inject usersService in user components

--- src/app/active-users/active-users.component.ts
+++ src/app/active-users/active-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {UsersServiceService} from '../shared/users-service.service';
 
 @Component({
   selector: 'app-active-users',
@@ -6,6 +7,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./active-users.component.css']
 })
 export class ActiveUsersComponent {
+
+  constructor(
+    private usersService: UsersServiceService
+  ) {}
 
   onSetToInactive(id: number) {
     this.userSetToInactive.emit(id);
--- src/app/inactive-users/inactive-users.component.ts
+++ src/app/inactive-users/inactive-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {UsersServiceService} from '../shared/users-service.service';
 
 @Component({
   selector: 'app-inactive-users',
@@ -6,6 +7,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./inactive-users.component.css']
 })
 export class InactiveUsersComponent {
+
+  constructor(
+    private usersService: UsersServiceService
+  ) {}
 
   onSetToActive(id: number) {
     this.userSetToActive.emit(id);
