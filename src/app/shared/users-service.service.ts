9195945d558f5c997d537716f6c13cb3f33cf9c5
Sascha <sascha.picard@googlemail.com>
2019-10-19 13:34:28 +0200

    move data and logic from appComponent to usersService

--- src/app/shared/users-service.service.ts
+++ src/app/shared/users-service.service.ts
@@ -5,5 +5,18 @@ import { Injectable } from '@angular/core';
 })
 export class UsersServiceService {
 
+  activeUsers = ['Max', 'Anna'];
+  inactiveUsers = ['Chris', 'Manu'];
+
   constructor() { }
+
+  setToInactive(id: number) {
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
+  }
+
+  setToActive(id: number) {
+    this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1);
+  }
 }
