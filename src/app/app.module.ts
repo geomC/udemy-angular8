a3a1daa8f5091a8b623c8ad456a36ae0650fe28a
Sascha <sascha.picard@googlemail.com>
2019-10-19 13:37:15 +0200

    provide new Services in appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ActiveUsersComponent } from './active-users/active-users.component';
 import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+import {UsersServiceService} from './shared/users-service.service';
+import {CountersServiceService} from './shared/counters-service.service';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,7 @@ import { InactiveUsersComponent } from './inactive-users/inactive-users.componen
     BrowserModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [UsersServiceService, CountersServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
