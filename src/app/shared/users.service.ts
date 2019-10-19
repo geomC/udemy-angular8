ae3bbed6caf759cd6e5e41587d90a16aee47b49d
Sascha <sascha.picard@googlemail.com>
2019-10-19 13:47:26 +0200

    inject counterService into userService and add countLogic to user methods

--- src/app/shared/users.service.ts
+++ src/app/shared/users.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {CounterService} from './counter.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,19 @@ export class UsersService {
   activeUsers = ['Max', 'Anna'];
   inactiveUsers = ['Chris', 'Manu'];
 
-  constructor() { }
+  constructor(
+    private countersService: CounterService
+  ) { }
 
   setToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
+    this.countersService.countActiveToInactive();
   }
 
   setToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
+    this.countersService.countInactiveToActive();
   }
 }
