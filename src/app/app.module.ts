d450a849e137c950c223fbf998d5b6a4776c0150
Sascha <sascha.picard@googlemail.com>
2019-10-20 12:48:31 +0200

    declare route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,22 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '', // index route
+    component: HomeComponent
+  },
+  {
+    path: 'users', // localhost:4200/users. IMPORTANT: don't prefix route with slash
+    component: UserComponent
+  },
+  {
+    path: 'servers', // localhost:4200/users. IMPORTANT: don't prefix route with slash
+    component: ServersComponent
+  },
+];
 
 @NgModule({
   declarations: [
@@ -25,6 +41,7 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(routes) // register routes
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
