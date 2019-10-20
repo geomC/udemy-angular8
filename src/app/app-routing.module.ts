2b6ac538471854b244d4c6f6e55b4feddf57eb6e
Sascha <sascha.picard@googlemail.com>
2019-10-20 20:56:47 +0200

    chore: factor out routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './servers/server/server.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent, children: [
+      { path: ':id', component: UserComponent },
+    ]
+  },
+  { path: 'servers', component: ServersComponent, children: [
+      // note that for the child routes we need to add a router outlet in ServersComponent !
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent },
+    ]
+  },
+  {
+    path: 'not-found', // path under which a 404 feedback component is reachable
+    component: PageNotFoundComponent
+  },
+  {
+    path: '**', // catch all unknown paths not handled by the other routes before. --> here the order matters!
+    redirectTo: 'not-found' // redirect to another path
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule] // from this module, if I were to add this module to the imports list of
+  // another module, what should be accessible to the consuming module
+})
+export class AppRoutingModule {
+
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,30 +11,9 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent, children: [
-      { path: ':id', component: UserComponent },
-    ]
-  },
-  { path: 'servers', component: ServersComponent, children: [
-      // note that for the child routes we need to add a router outlet in ServersComponent !
-      { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent },
-    ]
-  },
-  {
-    path: 'not-found', // path under which a 404 feedback component is reachable
-    component: PageNotFoundComponent
-  },
-  {
-    path: '**', // catch all unknown paths not handled by the other routes before. --> here the order matters!
-    redirectTo: 'not-found' // redirect to another path
-  }
-];
 
 @NgModule({
   declarations: [
@@ -50,7 +29,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(routes) // register routes
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
