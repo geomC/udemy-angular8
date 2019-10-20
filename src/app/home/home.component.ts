f0923da8eaa95a429f677a5e42de8ec5fc4ca25c
Sascha <sascha.picard@googlemail.com>
2019-10-20 13:24:18 +0200

    add example to route dynamically

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  constructor(
+    private router: Router // inject Router
+  ) {}
 
-  ngOnInit() {
+  onLoadServers() {
+    // do some complex calculations or do something async ...
+    // now navigate
+    this.router.navigate(
+      ['/servers'] // elements of a path
+    );
   }
-
 }
