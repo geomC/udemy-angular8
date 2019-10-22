467f7cc80493ebe00b3ad2f57d0e83b52fd9d7e3
Sascha Picard <sascha.picard@fit.fichtner.de>
2019-10-22 09:15:13 +0200

    link recipe-edit component with new route /recipes/new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
+import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
 
 const routes: Routes = [
   {
@@ -23,6 +24,10 @@ const routes: Routes = [
       {
         path: ':id',
         component: RecipeDetailComponent
+      },
+      {
+        path: 'new',
+        component: RecipeEditComponent
       }
     ]
   },
