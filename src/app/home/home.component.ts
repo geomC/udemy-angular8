1eaf0667ffb2bdb53b0488d34871166daea2649a
Sascha Picard <sascha.picard@fit.fichtner.de>
2019-10-25 09:04:21 +0200

    "fix": end subscription

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private intervalSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
-    interval(1000) // number in milisecs, just like setInterval
+    // store the Subscription returned by subscribe() so we end the subscription in onDestroy()
+    this.intervalSubscription = interval(1000)
       .subscribe( (count: number) => {
           console.log(`new count: ${count}`);
       });
+  }
+
+  ngOnDestroy() {
+    this.intervalSubscription.unsubscribe();
   }
 
 }
