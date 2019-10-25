bec72eda6a245e5ec698b0efea7409d08b586ea6
Sascha Picard <sascha.picard@fit.fichtner.de>
2019-10-25 16:21:56 +0200

    create new FormGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  genders = ['male', 'female', 'diverse'];
+
+  signupForm: FormGroup;
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null), // wrapped in strings so it does not get mangled during minifcation
+      'email': new FormControl(null),
+      'gender': new FormControl(this.genders[1])
+    });
+  }
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule // contains all the tools the generate the form
   ],
   providers: [],
   bootstrap: [AppComponent]
