a5c17a8a66dab60abc5733e1670befb522223aef
Sascha Picard <sascha.picard@fit.fichtner.de>
2019-10-25 15:23:19 +0200

    complete assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  suggestUserName() {
-    const suggestedName = 'Superuser';
+
+  subscriptionTypes = [
+    'Basic',
+    'Advanced',
+    'Pro'
+  ];
+
+  formStatePrint;
+
+  defaultSubscriptionType = this.subscriptionTypes[1];
+
+  submit(f: NgForm) {
+    const formState = f.value;
+    this.formStatePrint = JSON.stringify(formState, null, `t`);
+    console.log(`FORM STATE: ${ this.formStatePrint}`);
+    f.reset();
   }
 }
