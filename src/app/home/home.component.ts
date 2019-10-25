ce35d0b8d20e66b4d409684d26afdbf60099a01c
Sascha Picard <sascha.picard@fit.fichtner.de>
2019-10-25 10:06:41 +0200

    use rxjs operators

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,42 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {interval, Subscription} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
+import {filter, map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit {
 
   private intervalSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
-    // store the Subscription returned by subscribe() so we end the subscription in onDestroy()
-    this.intervalSubscription = interval(1000)
-      .subscribe( (count: number) => {
-          console.log(`new count: ${count}`);
-      });
-  }
+    const customObservable = new Observable(observer => { // part that is interested in stream updates/errors/completion
+      let count = 0;
+      setInterval(() => {
+        observer.next(count); // emit a value
+        count++;
 
-  ngOnDestroy() {
-    this.intervalSubscription.unsubscribe();
-  }
+        if (count === 4) {
+          observer.complete();
+        }
 
+      }, 1000);
+    });
+
+
+    this.intervalSubscription = customObservable.pipe(
+      filter(data => data > 0)
+      , map((data) => { // current emitted data
+        return `Round: ${(data as string) + 1}`;
+      })
+    ).subscribe({
+      next: console.log.bind(console),
+      error: console.error.bind(console),
+      complete: () => console.log('DONE COUNTING')
+    });
+  }
 }
