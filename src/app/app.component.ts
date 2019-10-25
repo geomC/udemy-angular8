b0a1642b3721d3c9efaa202a9cd7b5c6f68c1c72
Sascha <sascha.picard@googlemail.com>
2019-10-25 22:00:21 +0200

    add FormGroup and base validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators as V } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  projectsForm: FormGroup;
+
+  private projectStates = ['Stable', 'Critical', 'Finished'];
+
+  ngOnInit() {
+    this.projectsForm = new FormGroup({
+      'name': new FormControl(null, V.required),
+      'email': new FormControl(null, [V.required, V.email]),
+      'status': new FormControl(this.projectStates[0],[V.required, V.email])
+    });
+  }
+
 
 }
