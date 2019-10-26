b27e5e97fc4d1213f60278f87a490bb225e56e7a
Sascha <sascha.picard@googlemail.com>
2019-10-26 12:43:19 +0200

    refactor(ShoppingList): re-arrange code

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
@@ -29,17 +29,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     );
   }
 
-  private onNewItemEdit() {
-    return (index: number) => {
-      this.editMode = true;
-      this.editedItemIndex = index;
-      this.editedItem = this.shoppingListService.getIngredient(index);
-      this.slForm.setValue(this.editedItem);
-    };
-  }
-
   onFormSubmit(form: NgForm) {
-    const { name, amount} = form.value;
+    const {name, amount} = form.value;
     const newIngredient = new Ingredient(name, amount);
     if (this.editMode) {
       this.updateItem(this.editedItemIndex, newIngredient);
@@ -49,16 +40,25 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.resetForm();
   }
 
+  ngOnDestroy() {
+    this.startEditSub.unsubscribe();
+  }
+
+  private onNewItemEdit() {
+    return (index: number) => {
+      this.editMode = true;
+      this.editedItemIndex = index;
+      this.editedItem = this.shoppingListService.getIngredient(index);
+      this.slForm.setValue(this.editedItem);
+    };
+  }
+
   private addItem(newIngredient: Ingredient) {
     this.shoppingListService.addIngredient(newIngredient);
   }
 
   private updateItem(editedItemIndex: number, newIngredient: Ingredient) {
     this.shoppingListService.updateIngredient(editedItemIndex, newIngredient);
-  }
-
-  ngOnDestroy() {
-    this.startEditSub.unsubscribe();
   }
 
   private resetForm() {
