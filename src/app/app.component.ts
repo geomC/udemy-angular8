191ed4cc66e6da2381ec2ffeb885a1aa349cc0e6
Sascha <sascha.picard@googlemail.com>
2019-10-27 21:23:15 +0100

    typing the GET response body

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+interface Post {
+  title: string;
+  content: string;
+  id?: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,31 +14,50 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
+
+  FIREBASE_ENDPOINT_URL = 'https://udemy-angular8-course-backend.firebaseio.com/posts.json';
 
   constructor(
     private http: HttpClient
-  ) {}
+  ) {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     this.http
-      .post('https://udemy-angular8-course-backend.firebaseio.com/posts.json', postData)
+      .post(this.FIREBASE_ENDPOINT_URL, postData)
       // if no subscription is configured, no request is sent
       .subscribe(responseData => { // ng extracts the response data if not configured otherwose
         console.log(responseData);
       });
-      // no unsubscribe in component teardown necessary since the observable is baked-in in N
+    // no unsubscribe in component teardown necessary since the observable is baked-in in N
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
   }
 
+  private fetchPosts() {
+    this.http.get<{[key: string]: Post}> // set the response body type by making usage of the generic character of get
+    (this.FIREBASE_ENDPOINT_URL)
+      .pipe(
+        map( (responseData) => Object.keys(responseData)
+          // data is a nested object, we want to store it as array
+            .map(internalIdKey => ({ ...responseData[internalIdKey], id: internalIdKey}))
+        )
+      )
+      .subscribe( (posts) => {
+        console.log(posts);
+        this.loadedPosts = posts;
+      });
+  }
 }
