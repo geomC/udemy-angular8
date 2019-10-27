b700db0e33602233323be29aecb8354d77e32967
Sascha <sascha.picard@googlemail.com>
2019-10-27 20:58:23 +0100

    add post req

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,21 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {}
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-
+    this.http
+      .post('https://udemy-angular8-course-backend.firebaseio.com/posts.json', postData)
+      // if no subscription is configured, no request is sent
+      .subscribe(responseData => { // ng extracts the response data if not configured otherwose
+        console.log(responseData);
+      });
+      // no unsubscribe in component teardown necessary since the observable is baked-in in N
   }
 
   onFetchPosts() {
@@ -25,4 +33,5 @@ export class AppComponent implements OnInit {
   onClearPosts() {
     // Send Http request
   }
+
 }
