17bfa8f7981027148f687c9e2b33cc65c53e3cd6
Sascha <sascha.picard@googlemail.com>
2019-10-30 22:03:12 +0100

    "fix" ts bug to be able to build

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -28,6 +28,10 @@ export class RecipeEditComponent implements OnInit {
     });
   }
 
+  get ingredientsControls() {
+    return (this.recipeForm.get('ingredients') as FormArray).controls;
+  }
+
   onSubmit() {
     // const newRecipe = new Recipe(
     //   this.recipeForm.value['name'],
