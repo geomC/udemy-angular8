54edca45d8ce3e62220e63c22534d46993d14215
Sascha <sascha.picard@googlemail.com>
2019-10-30 16:04:32 +0100

    close modal on button/backdrop click

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
@@ -8,10 +8,15 @@ import { Component, Input, OnInit } from '@angular/core';
 export class AlertComponent implements OnInit {
 
   @Input() message: string = null;
+  @Output() close = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClose() {
+    this.close.emit();
   }
 
 }
