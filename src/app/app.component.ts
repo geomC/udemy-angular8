8c65122fbad24f569c62cc5865c3536b49de9efe
Sascha <sascha.picard@googlemail.com>
2019-11-01 21:27:34 +0100

    use void to animate elements getting added or removed from the dom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -58,6 +58,26 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
           animate(500) // no props passed. this removes the border radius
         ])
       ]
+    ),
+    trigger(
+      'list1',
+      [
+        // how should it look like when it IS in the dom
+        state('in', // in is only a dummy name it is not used in the template.
+          style({opacity: 1, transform: 'translateX(0)'})
+        ),
+        // how should it look like when it is added to the dom
+        transition('void => *', [ // void tells NG "when this element is not been added to the dom yet OR when it has been removed"
+            style({opacity: 0, transform: 'translateX(-100px)'}), // this style is added right in the beginning
+            animate(300)
+          ] // take some time to apply the final style
+        ),
+        transition('* => void', [
+            animate(300),
+            style({opacity: 0, transform: 'translateX(100px)'})
+          ]
+        )
+      ]
     )
   ]
 })
@@ -78,5 +98,9 @@ export class AppComponent {
 
   onShrink() {
     this.wildState = 'shrunken';
+  }
+
+  onDelete(index: number) {
+    this.list.splice(index, 1);
   }
 }
