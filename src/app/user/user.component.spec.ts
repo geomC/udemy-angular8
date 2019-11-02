653d34153fbff81566ab203d6f6f2de862e14274
Sascha <sascha.picard@googlemail.com>
2019-11-02 13:29:38 +0100

    test service injection

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
+import { UserService } from './user.service';
 
 describe('UserComponent', () => {
   beforeEach(() => {
@@ -16,5 +17,15 @@ describe('UserComponent', () => {
     let app = fixture.debugElement // the element exposed to us for testing purposes
       .componentInstance;
     expect(app).toBeTruthy(); // does it exist
+  });
+
+  it('should should use the username from the service', () => {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement // the element exposed to us for testing purposes
+      .componentInstance;
+    // run change detection to update the template after the service is injected
+    fixture.detectChanges();
+    let userService = fixture.debugElement.injector.get(UserService)
+    expect(userService.user.name).toEqual(app.user.name);
   });
 });
