d762f20225f3090ec3d76e0147e475eedfd05904
Sascha <sascha.picard@googlemail.com>
2019-11-03 19:41:40 +0100

    add ADD_INGREDIENTS action

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -2,6 +2,7 @@ import { Action } from '@ngrx/store';
 import { Ingredient } from '../../shared/ingredient.model';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT; // readonly tells other scripts to never override that value
@@ -12,4 +13,10 @@ export class AddIngredient implements Action {
   }
 }
 
-
+export class AddIngredients implements Action {
+  readonly type = ADD_INGREDIENTS;
+  constructor(
+    public payload: Ingredient[]
+  ) {
+  }
+}
