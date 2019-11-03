83b1ed8040eb1b04e05e2ecd9469243a6e6ec0b2
Sascha <sascha.picard@googlemail.com>
2019-11-03 19:36:52 +0100

    update addIngredientAction to take payload as arg

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -5,5 +5,11 @@ export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT; // readonly tells other scripts to never override that value
-  payload: Ingredient; // to use a payload property is just a convention. The Action interface only states a type property
+
+  constructor(
+    public payload: Ingredient // to use a payload property is just a convention. The Action interface only states a type property
+  ) {
+  }
 }
+
+
