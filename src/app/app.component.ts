fe8bc1619f122c187c27c5fa82b13821705baafc
Sascha <sascha.picard@googlemail.com>
2019-11-03 13:15:17 +0100

    show that binding innerHtml to a custom html element does not render anything since the tag us unnkown to the browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,13 @@ export class AppComponent {
 
   constructor() {
     setTimeout(() => {
-      this.content = 'html paragraph';
-    }, 1000);
+      // this is not rendered since the element is not known to the browser.
+      // At the time this is added to the dom, ng will not consider it for rendering.
+      this.content = '<app-alert message="Rendered dynamically"></app-alert>';
+    }, 1000); // the timeout is just to simulate an async data flow, e.g. an api call
+
+    // this also won't work here
+    this.content = '<app-alert message="Rendered dynamically"></app-alert>';
   }
 
 }
