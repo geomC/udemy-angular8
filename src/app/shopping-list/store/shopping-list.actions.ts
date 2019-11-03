c05b0ea65e4c6e17be367028648a3fcc9c960339
Sascha <sascha.picard@googlemail.com>
2019-11-03 19:59:30 +0100

    add actions UPDATE_INGREDIENT and DELETE_INGREDIENT

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -3,6 +3,8 @@ import { Ingredient } from '../../shared/ingredient.model';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT; // readonly tells other scripts to never override that value
@@ -15,11 +17,33 @@ export class AddIngredient implements Action {
 
 export class AddIngredients implements Action {
   readonly type = ADD_INGREDIENTS;
+
   constructor(
     public payload: Ingredient[]
   ) {
   }
 }
 
+export class Updateingredient implements Action {
+  readonly type = UPDATE_INGREDIENT;
+
+  constructor(
+    public payload: { index: number, ingredient: Ingredient }
+  ) {
+  }
+}
+
+export class DeleteIngredient implements Action {
+  readonly type = DELETE_INGREDIENT;
+
+  constructor(
+    public payload: number
+  ) {
+  }
+}
+
 // export a union type --> a type that combines the props of the stated types
-export type ShoppingListActions = AddIngredient | AddIngredients;
+export type ShoppingListActions = AddIngredient |
+  AddIngredients |
+  Updateingredient |
+  DeleteIngredient;
